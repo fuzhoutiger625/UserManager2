6d05df3f388e837e864c133b4654df05af2bf901
Administrator <Administrator@CN-20190509THSZ>
2020-01-09 19:38:53 +0800

    add

--- src/com/hsp/view/LoginServlet.java
+++ src/com/hsp/view/LoginServlet.java
@@ -1,8 +1,12 @@
 package com.hsp.view;
 import com.hsp.view.LoginServlet;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,11 +18,14 @@ public class LoginServlet extends HttpServlet {
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     response.setContentType("text/html");
     response.setCharacterEncoding("UTF-8");
+    String id = getId(request, response);
+
     response.getWriter().append("<img src='imgs/tomcat.gif' /><hr/>");
     response.getWriter().append("<h1>用户登录</h1>");
     response.getWriter().append("<form action='/UserManager2/LoginClServlet' mechod='post'>");
-    response.getWriter().append("用户名：<input type='text' name='id'/><br>");
+    response.getWriter().append("用户名：<input type='text' name='id' value='"+id+"'/><br>");
     response.getWriter().append("密码：<input type='password' name='password'/><br>");
+    response.getWriter().append("<input type='checkbox' value='keep' name='iskeepinfo'/>在此电脑上保留用户名<br>");
     response.getWriter().append("<input type='submit' value='登录'/><br>");
     response.getWriter().append("</form>");
     String errInfo = (String)request.getAttribute("err");
@@ -28,5 +35,22 @@ public class LoginServlet extends HttpServlet {
   
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     doGet(request, response);
+  }
+  
+  String getId(HttpServletRequest request, HttpServletResponse response) {
+	  String id = "";
+	  boolean b = false;
+	    Cookie [] cookies = request.getCookies();
+	    if(cookies != null) {
+		    for(int i=0;i<cookies.length;i++) {
+		    	Cookie cookie = cookies[i];
+		    	if("id".equals(cookie.getName())){
+		    		id = cookie.getValue();
+		    		b = true;
+		    		
+		    	}
+		    }
+	    }
+	    return id;
   }
 }
--- src/com/hsp/view/MainFrame.java
+++ src/com/hsp/view/MainFrame.java
@@ -1,8 +1,12 @@
 package com.hsp.view;
 import com.hsp.view.MainFrame;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,7 +18,24 @@ public class MainFrame extends HttpServlet {
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     response.setContentType("text/html");
     response.setCharacterEncoding("UTF-8");
-    response.getWriter().append("<img src='imgs/tomcat.gif' /> <a href=/UserManager2/LoginServlet>返回重新登录</a><br>");
+    String val = request.getParameter("iskeepinfo");
+    String id = request.getParameter("id");
+    String password = request.getParameter("password");
+    if("keep".equals(val)) {
+    	Cookie cookie = new Cookie("id", id);
+    	cookie.setMaxAge(7*24*60*60);
+    	response.addCookie(cookie);
+    }else {
+    	Cookie[] cookies = request.getCookies();
+    	for(Cookie cookie:cookies) {
+    		if("id".equals(cookie.getName())) {
+    			cookie.setMaxAge(0);
+    			response.addCookie(cookie);
+    		}
+    	}
+    }
+    String welcome = GetWelcome(request, response);
+    response.getWriter().append("<img src='imgs/tomcat.gif' />欢迎用户 "+id+" 登录，" + welcome + "<a href=/UserManager2/LoginServlet>返回重新登录</a><br>");
     //response.getWriter().append("<h3><br>");
     response.getWriter().append("<a href=/UserManager2/ManageUsers>用户管理</a><br>");
     response.getWriter().append("<a href=/UserManager2/UserClServlet?type=gotoAddUser>添加用户</a><br>");
@@ -24,5 +45,30 @@ public class MainFrame extends HttpServlet {
   
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     doGet(request, response);
+  }
+  
+  String GetWelcome(HttpServletRequest request, HttpServletResponse response) {
+	  boolean b = false;
+	  String wel = "";
+	    Cookie [] cookies = request.getCookies();
+	    if(cookies != null) {
+		    for(int i=0;i<cookies.length;i++) {
+		    	Cookie cookie = cookies[i];
+		    	if("lasttime".equals(cookie.getName())){
+		    		wel = "您上次登录的时间是:" + cookie.getValue();
+		    		b = true;
+		    	}
+		    }
+	    }
+	    if(!b) {
+	    	wel = "这是您第一次登录";
+	    }
+	    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd===HH:mm:ss");
+	    String nowTime = simpleDateFormat.format(new Date());
+	    Cookie cookie = new Cookie("lasttime", nowTime);
+	    cookie.setMaxAge(7 * 24 * 60 * 60);
+	    //C:\Users\Administrator\AppData\Local\Microsoft\Windows\Temporary Internet Files
+	    response.addCookie(cookie);
+	    return wel;
   }
 }
